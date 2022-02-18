d627c36c5a8595cfbf85beedd2d36ade5486f25f
BuildTools <unconfigured@null.spigotmc.org>
2022-02-18 21:28:36 +0100

    Autocloseables now in Try-resources and waiting till Thread is finished

--- FirstWebChatAppTry/src/Client.java
+++ FirstWebChatAppTry/src/Client.java
@@ -1,55 +1,81 @@
 
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.io.PrintWriter;
-    import java.net.Socket;
-    import java.util.Scanner;
-    
-    public class Client {
-        public static void main(String[] args) {
-            try {
-                Socket clientSocket = new Socket("localhost", 5000); // ! IP, Port
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream()); // ? Output Printer (Server)
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // ? ReadInput
-    
-                // ? New Thread
-                Thread sender = new Thread(new Runnable() { // ? Anonymus (new) Runnable
-                    String msg;
-    
-                    @Override // ! Override default Runnable
-                    public void run() { // ? Run the Runnable
-                        try (Scanner sc = new Scanner(System.in)) {
-                            while (clientSocket.isConnected()) { // ? If connected allow Input
-                                msg = sc.nextLine(); // * Input
-                                out.println(msg); // * Output
-                                out.flush(); // ! Clears Printstream from leftover objects
-                            }
-                        }
-                    }
-                });
-                sender.start(); // ? Start Thread
-    
-                Thread receiver = new Thread(new Runnable() {
-                    String msg;
-    
-                    @Override
-                    public void run() {
-                        try {
-                            while ((msg = in.readLine()) != null)
-                                System.out.printf("Server: %s %n", msg);
-    
-                            System.out.println("Server out of Service");
-                            out.close();
-                            clientSocket.close();
-                        } catch (IOException io) {
-                            System.out.println("IOException: " + io.getMessage());
-                        }
-                    }
-                });
-                receiver.start();
-            } catch (IOException io) {
-                System.out.println("IOException: " + io.getMessage());
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private static final class Send implements Runnable {
+        private final PrintWriter out;
+        private final Socket clientSocket;
+        String msg;
+
+        private Send(PrintWriter out, Socket clientSocket) {
+            this.out = out;
+            this.clientSocket = clientSocket;
+        }
+
+        @Override // ! Override default Runnable
+        public void run() { // ? Run the Runnable
+            try (Scanner sc = new Scanner(System.in)) {
+                while (clientSocket.isConnected()) { // ? If connected allow Input
+                    msg = sc.nextLine(); // * Input
+                    out.println(msg); // * Output
+                    out.flush(); // ! Clears Printstream from leftover objects
+                }
             }
         }
-    } 
+    }
+
+    private static final class Receive implements Runnable {
+        private final BufferedReader in;
+        private final PrintWriter out;
+        private final Socket clientSocket;
+        String msg;
+
+        private Receive(BufferedReader in, PrintWriter out, Socket clientSocket) {
+            this.in = in;
+            this.out = out;
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while ((msg = in.readLine()) != null)
+                    System.out.printf("Server: %s %n", msg);
+
+                System.out.println("Server out of Service");
+                out.close();
+                clientSocket.close();
+            } catch (IOException io) {
+                System.err.println("IOException: " + io.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Socket clientSocket = new Socket("localhost", 5000)) {
+            try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream())) {
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                    Thread sender = new Thread(new Send(out, clientSocket));
+                    sender.start();
+
+                    Thread receiver = new Thread(new Receive(in, out, clientSocket));
+                    receiver.start();
+
+                    receiver.join();
+                    sender.join();
+                }
+            }
+        } catch (IOException io) {
+            System.err.println("IOException: " + io.getMessage());
+        } catch (InterruptedException e) {
+            System.err.println("InterruptedException: " + e.getMessage());
+            Thread.currentThread().interrupt();
+        }
+    }
+}
